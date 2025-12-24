eeb2be64a4444366697b7f3e266ff43e0ff39f37
Johan Contreras <johancontreras.alxlabs@gmail.com>
2025-12-24 13:55:30 -0500

    db: creacion de base de datos y tablas

---
+++ scripts/db/V1__create_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE envios_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
---
+++ scripts/db/V2__create_tables.sql
@@ -0,0 +1,47 @@
+CREATE TABLE usuario (
+    id_usuario integer PRIMARY KEY,
+    nombre varchar(50) NOT NULl,
+    correo varchar(50) NOT NULL,
+    usuario varchar(20) NOT NULL,
+    contrasenia varchar(100) NOT NULL,
+    rol varchar(20) NOT NULL,
+    estado varchar(10) NOT NULL
+);
+
+CREATE TABLE cliente (
+    id_cliente integer PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    dni char(8) NOT NULL,
+    telefono varchar(20) NOT NULL,
+    correo varchar(50) NOT NULL
+);
+
+CREATE TABLE envio (
+    id_envio integer PRIMARY KEY,
+    id_cliente integer NOT NULL,
+    id_usuario integer NOT NULL,
+    codigo_envio varchar(10) NOT NULL,
+    origen varchar(20) NOT NULL,
+    destino varchar(20) NOT NULL,
+    peso float NOT NULL,
+    importe float NOT NULL,
+    fecha_envio timestamp NOT NULL,
+    dni_destinatario char(8) NOT NULL,
+    nombre_destinatario varchar(50) NOT NULL,
+    telefono_destinatario varchar(20) NOT NULL,
+
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE seguimiento (
+    id_seguimiento integer PRIMARY KEY,
+    id_envio integer NOT NULL,
+    origen varchar(20) NOT NULL,
+    destino varchar(20) NOT NULL,
+    fecha_salida timestamp NOT NULL,
+    fecha_llegada timestamp,
+    estatus varchar(20) NOT NULL,
+
+    FOREIGN KEY (id_envio) REFERENCES envio(id_envio)
+)
