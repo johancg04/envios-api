f34543df60d5e1497276a911b9817ea4f5d7b9a1
Johan Contreras <j.contreras.granados.04@gmail.com>
2026-01-25 21:26:24 -0500

    feat: hashear contrasenia y verificar ingreso

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -1,12 +1,17 @@
+import hashlib
+
 from app.models.usuario import UsuarioCrear, Usuario, UsuarioLogin
 from app.repositories.usuario_repository import usuario_repository
 
+def hashear_texto(texto : str) -> str:
+    return hashlib.sha256(texto.encode('utf-8')).hexdigest()
 
 class UsuarioService:
     def __init__(self):
         pass
 
     def crear(self, usuario_crear: UsuarioCrear) -> Usuario | dict:
+        usuario_crear.contrasenia = hashear_texto(usuario_crear.contrasenia)
         nuevo_usuario = usuario_repository.insertar(usuario_crear)
         if nuevo_usuario:
             return nuevo_usuario
@@ -21,6 +26,7 @@ class UsuarioService:
             return {"mensaje_error": "Error al listar usuarios"}
 
     def loguear(self, usuario_login: UsuarioLogin) -> dict:
+        usuario_login.contrasenia = hashear_texto(usuario_login.contrasenia)
         try:
             usuario_encontrado = usuario_repository.buscar(usuario_login)
             if usuario_encontrado:
